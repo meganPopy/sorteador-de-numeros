3a1e50b67aa8dd920c0ba050def47310fc5439c2
Tânia <106488757+meganPopy@users.noreply.github.com>
2024-11-02 21:37:35 -0300

    a

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-function sortear(){
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
-    
-    alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até o número: ${ate}`);
-}
 
+function sortear(){
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    
+    let sorteados = [];
+    let numero;
+    
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+    
+    alert(sorteados);
+}
